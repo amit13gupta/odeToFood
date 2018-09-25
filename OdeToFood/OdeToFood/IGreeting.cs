7d3a3b98d9810d491b38d7a35f0283d422093a5a
Amit Gupta <Amit.Gupta@nov.com>
2018-09-24 22:51:47 -0500

    commit

---
+++ OdeToFood/OdeToFood/IGreeting.cs
@@ -0,0 +1,15 @@
+﻿namespace OdeToFood
+{
+    public interface IGreeting
+    {
+        string GreetingOfTheDay();
+    }
+
+    public class Greeting : IGreeting
+    {
+        public string GreetingOfTheDay()
+        {
+            return "greeting of the day";
+        }
+    }
+}
--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -15,19 +17,22 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeting, Greeting>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeting greeting,ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+               app.UseDeveloperExceptionPage();
             }
-
+            
+        
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greetings = greeting.GreetingOfTheDay();
+                await context.Response.WriteAsync($"{greetings}" + $"{env.EnvironmentName}");
             });
         }
     }
